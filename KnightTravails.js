a80d59fc43890fb6d8f009540859b8ae139bc092
Zulu0824 <sriharsha.patta@gmail.com>
2026-09-25 10:36:08 +0530

    Add knightMoves(x,y) function

--- KnightTravails.js
+++ KnightTravails.js
@@ -14,3 +14,31 @@ export function getMoves([x, y]) {
     .map(([dx, dy]) => [x + dx, y + dy])
     .filter(([nx, ny]) => nx >= 0 && nx < 8 && ny >= 0 && ny < 8);
 }
+
+function knightMoves(start, end) {
+  if (start.toString() === end.toString) {
+    console.log("Knight is in the same place");
+    return;
+  }
+
+  const queue = [[start]];
+  const visited = [start.toString()];
+  while (queue.length > 0) {
+    const path = queue.shift();
+    const current = path[path.length - 1];
+
+    if (current[0] === end[0] && current[1] === current[1]) {
+      console.log(`You made it in ${path.length - 1} moves! Here's your Path:`);
+      path.forEach((square) => console.log(`[${square[0]}, ${square[1]}]`));
+      return path;
+    }
+
+    const moves = getMoves(current);
+    for (const move of moves) {
+      if (!visited.includes(move.toString())) {
+        visited.push(move.toString);
+        queue.push([...path, move]);
+      }
+    }
+  }
+}
